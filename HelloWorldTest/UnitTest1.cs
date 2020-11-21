2b49bc5677ab2a9f924578f9ddaffbb08027e71c
skY <1530321852@qq.com>
2020-11-21 14:18:06 +0800

    创建项目

---
+++ HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using HelloWorld.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        [TestMethod]
+        [TestCategory("≥£”√")]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController(_logger);
+
+            JsonResult result = controller.GetGuid();
+
+            Console.WriteLine(result.Value);
+        }
+    }
+}
