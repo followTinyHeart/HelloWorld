8375465f0ea8fdfaecb495a66edce368a8741f72
skY <1530321852@qq.com>
2021-07-21 14:17:01 +0800

    feat(test):新增

---
+++ HelloWorldTest/TreeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeItemBank.Controllers;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class TreeControllerTest
+    {
+        [TestMethod]
+        [TestCategory("树")]
+        public void TestMethod() 
+        {
+
+        }
+    }
+}
