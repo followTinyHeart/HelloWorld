bea5a0b0d464789e96e3f46d95bbd4eead3eb5cc
HONOR <1530321852@qq.com>
2022-04-24 16:57:57 +0800

    feat(controller):BasicController

---
+++ HelloWorld/Controllers/BasicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    /// <summary>
+    /// 语法测试.
+    /// </summary>
+    public class BasicController : Controller
+    {
+       
+    }
+}
